7982cb0632ee587b0345b8f2382bf90e7ff2177f
jeppeter <jeppeter@gmail.com>
2018-07-02 14:29:10 +0800

    to add keyexp exception

--- src/extargsparse/keycls.cs
+++ src/extargsparse/keycls.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace extargsparse
 {
-    public class KeyCls
-    {
-    }
+public class KeyCls
+{
+    private static readonly Regex m_helpexpr = new Regex("##([^\\#\\!\\+\\<\\>\\|]+)##$", RegexOptions.IgnoreCase);
+    private static readonly Regex m_cmdexpr = new Regex("^([^\\#\\<\\>\\+\\$\\!]+)", RegexOptions.IgnoreCase);
+    private static readonly Regex m_prefixexpr = new Regex("\\+([a-zA-Z]+[a-zA-Z_\\-0-9]*)", RegexOptions.IgnoreCase);
+    private static readonly Regex m_funcexpr = new Regex("<([^\\<\\>\\#\\$\\| \\t\\!]+)>", RegexOptions.IgnoreCase);
+    private static readonly Regex m_flagexpr = new Regex("^([a-zA-Z_\\|\\?\\-]+[a-zA-Z_0-9\\|\\?\\-]*)", RegexOptions.IgnoreCase);
+    private static readonly Regex m_mustflagexpr = new Regex("^\\$([a-zA-Z_\\|\\?]+[a-zA-Z_0-9\\|\\?\\-]*)",RegexOptions.IgnoreCase);
+    private static readonly Regex m_attrexpr = new Regex("\\!([^\\<\\>\\$!\\#\\|]+)\\!");
+}
 }
---
+++ src/extargsparse/keyexp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace extargsparse
+{
+	public class KeyException : Exception
+	{
+		public KeyException(string str) : base(str)
+		{
+			
+		}
+	}
+}
