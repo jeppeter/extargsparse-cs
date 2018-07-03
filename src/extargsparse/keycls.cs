98100b773f56dc702796e859938451234c45c1a6
jeppeter <jeppeter@gmail.com>
2018-07-03 09:57:26 +0800

    to add keyattr class

--- src/extargsparse/keycls.cs
+++ src/extargsparse/keycls.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-
+using System.Collections.Generic;
 
 namespace extargsparse
 {
@@ -13,5 +13,37 @@ public class KeyCls
     private static readonly Regex m_flagexpr = new Regex("^([a-zA-Z_\\|\\?\\-]+[a-zA-Z_0-9\\|\\?\\-]*)", RegexOptions.IgnoreCase);
     private static readonly Regex m_mustflagexpr = new Regex("^\\$([a-zA-Z_\\|\\?]+[a-zA-Z_0-9\\|\\?\\-]*)",RegexOptions.IgnoreCase);
     private static readonly Regex m_attrexpr = new Regex("\\!([^\\<\\>\\$!\\#\\|]+)\\!");
+
+    protected class KeyAttr
+    {
+    	private string m_splitchar = ";";
+    	private Dictionary<string,string> m_obj;
+
+    	public KeyAttr(string instr)
+    	{
+    		char sc ;
+    		if (instr.ToLower().StartsWith("splitchar=") &&
+    			instr.Length >= 7) {
+    			sc = instr[6];
+    			if (sc == char('.')) {
+    				this.m_splitchar = ".";
+    			} else if (sc == char('\\')) {
+    				this.m_splitchar = "\\";
+    			}
+    		}
+    	}
+
+    	public string Attr(string key)
+    	{
+
+    	}
+
+    	public string ToString()
+    	{
+
+    	}
+    }
+
+
 }
 }
