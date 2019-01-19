1b726a95d3e23d21d7a76f1b46c66d75841241f8
jeppeter <jeppeter@gmail.com>
2019-01-19 13:10:04 +0800

    to add cmdpaths

--- src/extargsparse/parsestate.cs
+++ src/extargsparse/parsestate.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 namespace extargsparse
 {
-	public class _ParseState : _LogObject
+	public class _ParserState : _LogObject
 	{
-		public _ParseState(string[] args,_ParserCompact maincmd,ExtArgsOptions optattr=null) : base()
+		private List<_ParserCompact> m_cmdpaths;
+		public _ParserState(string[] args,_ParserCompact maincmd,ExtArgsOptions optattr=null) : base()
 		{
 			if (optattr == null) {
 				optattr = new ExtArgsOptions("{}");
 			}
+			this.m_cmdpaths = new List<_ParserCompact>();
+			this.m_cmdpaths.Add(maincmd);
 		}
 	}
 }
