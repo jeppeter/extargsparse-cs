51cb259fabf0f175824cb7098a07c78fd4c08a4a
jeppeter <jeppeter@gmail.com>
2019-01-19 19:13:27 +0800

    to add  priority

---
+++ src/extargsparse/priority.cs
@@ -0,0 +1,15 @@
+
+
+namespace extargsparse
+{
+	public enum Priority
+	{
+		COMMAND_SET  = 10,
+		SUB_COMMAND_JSON_SET = 20,
+		COMMAND_JSON_SET = 30,
+		ENVIRONMENT_SET = 40,
+		ENV_SUB_COMMAND_JSON_SET = 50,
+		ENV_COMMAND_JSON_SET = 60,
+		DEFAULT_SET = 70
+	}
+}
