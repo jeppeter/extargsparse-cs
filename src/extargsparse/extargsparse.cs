4b8b873a90d854d3e1e2544f1d7e3f3d6c118a32
jeppeter <jeppeter@gmail.com>
2019-01-20 15:01:47 +0800

    to add load map

--- src/extargsparse/extargsparse.cs
+++ src/extargsparse/extargsparse.cs
@@ -5,6 +5,9 @@ namespace extargsparse
 {
 	public class  ExtArgsParse  : _LogObject
 	{
+		private delegate bool load_command_line_func(string prefix,KeyCls keycls,List<_ParserCompact> curparser=null);
+		//private delegate int parse_action_func(NameSpaceEx args, int validx, KeyCls keycls,string[] params);
+
 		private static Priority[]  m_defprior = {Priority.COMMAND_SET,Priority.SUB_COMMAND_JSON_SET,Priority.COMMAND_JSON_SET,Priority.ENVIRONMENT_SET,Priority.ENV_SUB_COMMAND_JSON_SET,Priority.ENV_COMMAND_JSON_SET,Priority.DEFAULT_SET};
 		private Priority[] m_priority;
 		private ExtArgsOptions  m_options;
@@ -21,6 +24,80 @@ namespace extargsparse
 		private string m_jsonlong;
 		private bool m_cmdprefixadded;
 		private string m_errorhandler;
+		private Dictionary<string,load_command_line_func> m_loadcommandmap;
+		//private Dictionary<string,parse_action_func> m_optparsehandlemap;
+
+		private bool _load_command_line_base(string prefix,KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private bool _load_command_line_args(string prefix, KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private bool _load_command_line_help(string prefix, KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private bool _load_command_line_jsonfile(string prefix, KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private bool _load_command_line_subparser(string prefix, KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private bool _load_command_line_prefix(string prefix, KeyCls keycls, List<_ParserCompact> curparser=null)
+		{
+			return true;
+		}
+
+		private int _string_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 1;
+		}
+
+		private int _bool_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 0;
+		}
+
+		private int _int_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 1;
+		}
+
+		private int _append_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 1;
+		}
+
+
+		private int _inc_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 0;
+		}
+
+		private int _help_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 0;
+		}
+
+		private int _command_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 0;
+		}
+
+		private int _float_action(NameSpaceEx args, int validx, KeyCls keycls, string[] params)
+		{
+			return 1;
+		}
+
 
 		public ExtArgsParse(ExtArgsOptions options=null, object priority=null) : base()
 		{
@@ -52,6 +129,23 @@ namespace extargsparse
 			this.m_jsonlong = options.get_string("jsonlong");
 			this.m_cmdprefixadded = options.get_bool("cmdprefixadded");
 			this.m_errorhandler = options.get_string("errorhandler");
+
+			this.m_loadcommandmap = new Dictionary<string,load_command_line_func>();
+			this.m_loadcommandmap.Add("string", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("int", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("float", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("list", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("bool", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("args", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("command", new load_command_line_func(_load_command_line_subparser));
+			this.m_loadcommandmap.Add("prefix", new load_command_line_func(_load_command_line_prefix));
+			this.m_loadcommandmap.Add("count", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("help", new load_command_line_func(_load_command_line_base));
+			this.m_loadcommandmap.Add("jsonfile", new load_command_line_func(_load_command_line_base));
+
+			this.m_optparsehandlemap = new Dictionary<string,parse_action_func>();
+			this.m_optparsehandlemap.Add("string", new parse_action_func(_string_action));
+
 		}
 	}
 
